981188324fccc757453bca1223d8499ce8091534
Jackson Zou <jzou00@stuy.edu>
2018-12-13 10:48:16 -0500

    added methods to node

--- Node.java
+++ Node.java
@@ -3,4 +3,28 @@ public class Node{
   private Node next;
   private Node previous;
 
+  public Node next() {
+    return next;
+  }
+
+  public Node prev() {
+    return prev;
+  }
+
+  public void setNext(Node other) {
+    next = other;
+  }
+
+  public void setPrev(Node other) {
+    previous = other;
+  }
+
+  public Integer getData() {
+    return data;
+  }
+
+  public Integer setData(Integer i) {
+    data = i;
+  }
+
 }
